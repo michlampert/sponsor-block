21deb5ea8a7ba72ae1065ff4d6ac7cb9dc503c53
tmek1244 <tmek1244@wp.pl>
2022-11-05 11:43:07 +0100

    Working on github :)

--- src/background.js
+++ src/background.js
@@ -1,11 +1,36 @@
-function injectedFunction() {
+function injectedFunction(tabId) {
     console.log("test123")
+    alert("dasdasd")
     document.body.style.backgroundColor = 'orange';
 }
   
-chrome.action.onUpdated.addListener((tab) => {
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: injectedFunction
-    });
-});
+// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//     alert("test")
+//     if (changeInfo.status == 'complete' && tab.active) {
+  
+//       // do your things
+//       injectedFunction
+  
+//     }
+// });
+
+// const tabId = getTabId();
+// chrome.scripting.executeScript(
+//     {
+//       target: {tabId: tab.id, allFrames: true},
+//       files: ['script.js'],
+//     },
+//     () => { injectedFunction });
+// const tabId = getTabId();
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete') {
+        console.log(tabId, changeInfo)
+        // do your things
+
+    }
+})
+
+// chrome.tabs.onCreated.addListener(
+//     injectedFunction,
+// )
---
+++ src/content-scripts.js
@@ -0,0 +1,7 @@
+function injectedFunction(tabId) {
+    console.log("test123")
+    // alert("dasdasd")
+    document.body.style.backgroundColor = 'orange';
+}
+
+injectedFunction();
