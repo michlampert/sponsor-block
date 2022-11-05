c311574d0f533bc982f442d39f09f3ab5c419311
WojtAcht <wachtelik@gmail.com>
2022-11-05 15:49:57 +0100

    Add tensorflow model

--- sponsor-block/src/contentScript.js
+++ sponsor-block/src/contentScript.js
@@ -1,47 +1,48 @@
 'use strict';
 import * as tf from '@tensorflow/tfjs';
+import * as DICTIONARY from './dictionary.js';
 
 const ARTICLE_NESTING_LEVEL =  window.location.pathname === "/" ? 7 : 6;
 const firstArticle = document.querySelector('[role="article"]')
 console.log('first', firstArticle)
 
+const ENCODING_LENGTH = 20;
+
+function tokenize(wordArray) {
+  let returnArray = [DICTIONARY.START];
+  
+  for (var i = 0; i < wordArray.length; i++) {
+    let encoding = DICTIONARY.LOOKUP[wordArray[i]];
+    returnArray.push(encoding === undefined ? DICTIONARY.UNKNOWN : encoding);
+  }
+  while (i < ENCODING_LENGTH - 1) {
+    returnArray.push(DICTIONARY.PAD);
+    i++;
+  }
+  console.log([returnArray]);
+  return tf.tensor([returnArray]);
+}
 
 
-const MODEL_JSON_URL = chrome.runtime.getURL('model.json');
-// Set the minimum confidence for spam comments to be flagged.
-// Remember this is a number from 0 to 1, representing a percentage
-// So here 0.75 == 75% sure it is spam.
+const MODEL_JSON_URL = "https://raw.githubusercontent.com/michlampert/sponsor-block/feature/ts-model/sponsor-block/public/model.json"; // chrome.runtime.getURL("model.json");
+
 const SPAM_THRESHOLD = 0.75;
 
-// Create a variable to store the loaded model once it is ready so 
-// you can use it elsewhere in the program later.
 var model = undefined;
 
-/** 
- * Asynchronous function to load the TFJS model and then use it to
- * predict if an input is spam or not spam.
- */
 async function loadAndPredict(inputTensor) {
-  // Load the model.json and binary files you hosted. Note this is 
-  // an asynchronous operation so you use the await keyword
   if (model === undefined) {
     model = await tf.loadLayersModel(MODEL_JSON_URL);
   }
-  
-  // Once model has loaded you can call model.predict and pass to it
-  // an input in the form of a Tensor. You can then store the result.
   var results = await model.predict(inputTensor);
-  
-  // Print the result to the console for us to inspect.
-  console.log("dupa");
-  results.print();
+  const data = results.dataSync();
+  return data[0] > SPAM_THRESHOLD;
 }
 
-const prediction = loadAndPredict(tf.tensor([[1,3,12,18,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]));
-
-const shouldHide = (content) => Promise.resolve(
-  Math.random() > 0.5
-  )
+const shouldHide = async (content) => {
+  const lowercaseContentArray = content.toLowerCase().replace(/[^\w\s]/g, ' ').split(' ');
+  return await loadAndPredict(tokenize(lowercaseContentArray));
+};
 
 const articlesList = new Array(ARTICLE_NESTING_LEVEL)
     .fill(null)
