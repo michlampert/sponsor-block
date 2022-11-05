651a6776c8158cd2c006755d5e975a9ef194f475
KartonM <mkozubek@protonmail.com>
2022-11-05 12:33:39 +0100

    feat: Replace every second post with a red square

--- src/content-scripts.js
+++ src/content-scripts.js
@@ -1,9 +1,36 @@
-function main() {
-    console.log("test123")
-    document.body.style.backgroundColor = 'orange';
-    const text = document.body;
+const ARTICLE_NESTING_LEVEL = 6
 
-    // console.log(text)
-}
+const firstArticle = document.querySelector('[role="article"]')
 
-main();
+const shouldHide = (content) => Promise.resolve(Math.random() > 0.5)
+
+const articlesList = new Array(ARTICLE_NESTING_LEVEL)
+    .fill(null)
+    .reduce((node) => node.parentElement, firstArticle)
+
+const articlesListObserver = new MutationObserver((mutations => {
+    mutations.forEach((mutation) => {
+        if (mutation.type !== 'childList') {
+            return;
+        }
+        mutation.addedNodes.forEach((node) => {
+            const text = node.querySelector('[data-ad-comet-preview="message"]')?.textContent
+            if (text) {
+                shouldHide(text)
+                    .then((hide) => {
+                        if (hide) {
+                            console.log('hiding ', text)
+                            const redDiv = document.createElement("div")
+                            redDiv.style.height = '50px'
+                            redDiv.style.width = '50px'
+                            redDiv.style.backgroundColor = 'red'
+                            node.replaceChildren(redDiv)
+                        }
+                    })
+                console.log(text)
+            }
+        })
+    })
+}))
+
+articlesListObserver.observe(articlesList, { childList: true })
